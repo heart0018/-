209fae6c8d11fcca46e4e76d4c4bf2faed82a2c0
heart0018 <omiyanyan268@gmail.com>
2025-04-26 18:59:40 +0900

    最終更新

--- card.rb
+++ card.rb
@@ -1,1 +1,34 @@
-C
+class Card
+attr_reader :suit, :rank, :value
+
+def initialize(suit,rank)
+  @suit = suit
+  @rank = rank
+  @value  = value_rank(rank)
+end
+
+def to_s
+  if @suit == "Joker"
+    "ジョーカー"
+  else
+    "#{@suit}#{@rank}"
+  end
+end
+
+private #内部処理
+def value_rank(rank)
+  if suit == "Joker"
+    15 #ジョーカーは最強
+  end
+  #カードの値を数値に変換する
+  case rank
+  when "A" then 14
+  when "K" then 13
+  when "Q" then 12
+  when "J" then 11
+  else rank.to_i #1～10はそのまま数値
+  end
+end
+
+end
+
